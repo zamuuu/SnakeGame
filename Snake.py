76d58022ac471a7aa680ef7d2aa16ec1d4c30fcc
zamuuu <samuelcorreavallejo@gmail.com>
2025-02-05 17:39:17 -0300

    Implemented some images to know wich difficulty was selected

--- Snake.py
+++ Snake.py
@@ -14,9 +14,14 @@ clock = pygame.time.Clock()
 main_menu_BG = pygame.image.load('imgs/main_menu_background.png').convert_alpha()
 game_BG = pygame.image.load("imgs/game_background.jpg").convert_alpha()
 play_main_menu = pygame.image.load("imgs/play_button.png").convert_alpha()
+# Change difficulty images
 easy_difficulty = pygame.image.load("imgs/turtle.png").convert_alpha()
 normal_difficulty = pygame.image.load("imgs/rabbit.png").convert_alpha()
 hard_difficulty = pygame.image.load("imgs/snake.png").convert_alpha()
+clicked_easy_diff = pygame.image.load("imgs/turtle_clicked.png").convert_alpha()
+clicked_normal_diff = pygame.image.load("imgs/rabbit_clicked.png").convert_alpha()
+clicked_hard_diff = pygame.image.load("imgs/snake_clicked.png").convert_alpha()
+temp_blank = pygame.image.load("imgs/blank_temporary.png").convert_alpha()
 # Creating the Snake Object
 class Snake:
     def __init__(self):
@@ -77,13 +82,14 @@ def main_menu():
     selected_difficulty = 10  # Valor por defecto
     pygame.display.set_caption("Main Menu")
     click = False
-    click_difficulty = False
+    which_diff = temp_blank
+    difficulty_posx = 0
+    difficulty_posy = 0
     while True:
         # Main menu Background image
         screen.blit(main_menu_BG, (0,0))
         mousex, mousey = pygame.mouse.get_pos()
-        # Button to enter the game
-        
+
         # Play button
         play_main_menu_rect = play_main_menu.get_rect()
         play_main_menu_rect.center = (SW // 2, SH // 2)
@@ -92,27 +98,37 @@ def main_menu():
         easy_dificulty_r = easy_difficulty.get_rect()
         normal_dificulty_r = normal_difficulty.get_rect()
         hard_dificulty_r = hard_difficulty.get_rect()
+        
         # Positioning those bottons
-        easy_dificulty_r.topleft = (200, 350)
-        normal_dificulty_r.topleft = (350, 350)
-        hard_dificulty_r.topleft = (500, 350)
+        easy_dificulty_r.topleft = (200, 380)
+        normal_dificulty_r.topleft = (350, 380)
+        hard_dificulty_r.topleft = (500, 380)
+        
+        
+        screen.blit(play_main_menu, play_main_menu_rect)
+        screen.blit(easy_difficulty, (200, 380))
+        screen.blit(normal_difficulty, (350, 380))
+        screen.blit(hard_difficulty, (500, 380))
         
         
         if play_main_menu_rect.collidepoint(mousex, mousey) and click:
             game(selected_difficulty)
         elif easy_dificulty_r.collidepoint(mousex, mousey) and click:
             selected_difficulty = 6
+            which_diff = clicked_easy_diff
+            difficulty_posx, difficulty_posy = 200, 380
         elif normal_dificulty_r.collidepoint(mousex, mousey) and click:
             selected_difficulty = 10
+            which_diff = clicked_normal_diff
+            difficulty_posx, difficulty_posy = 350, 380
         elif hard_dificulty_r.collidepoint(mousex, mousey) and click:
             selected_difficulty = 20
-
-        screen.blit(play_main_menu, play_main_menu_rect)
-        screen.blit(easy_difficulty, (200, 350))
-        screen.blit(normal_difficulty, (350, 350))
-        screen.blit(hard_difficulty, (500, 350))
+            which_diff = clicked_hard_diff
+            difficulty_posx, difficulty_posy = 500, 380
         click = False
-
+        
+        screen.blit(which_diff, (difficulty_posx,difficulty_posy))
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
